23c790bcbe0fcad14aad738320a29ca3f1b1a300
rasshi200 <rasshisharma200@gmail.com>
2025-07-30 10:01:38 +0530

    Update Delete.jsx

--- my-client/src/components/Delete.jsx
+++ my-client/src/components/Delete.jsx
@@ -8,7 +8,7 @@ export function Delete() {
     async function deleteData(e){
         e.preventDefault();
         try{
-            const response = await axios.delete(`http://localhost:3001/api/employees/${id}`);
+            const response = await axios.delete(`https://my-project-ntck.onrender.com/api/employees/${id}`);
             alert(response.data.message);
         }
         catch(err){

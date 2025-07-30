9b9153b597374c73ccfc990af8871ecd4e6d67e3
rasshi200 <rasshisharma200@gmail.com>
2025-07-30 10:02:27 +0530

    Update Update.jsx

--- my-client/src/components/Update.jsx
+++ my-client/src/components/Update.jsx
@@ -11,7 +11,7 @@ const[empSal,setSal] = useState("");
 async function updateHandler(e){
     e.preventDefault();
     try{
-        const response = await axios.put(`http://localhost:3001/api/employees/${id}`,
+        const response = await axios.put(`https://my-project-ntck.onrender.com/api/employees/${id}`,
             {empNo:Number(empNo),
             empName,
             empSal:Number(empSal)
